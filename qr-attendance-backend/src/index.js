dd34a50602095c36b6c87a5143f234a972cf6771
Hande Nur Dalcalı <nhande276@gmail.com>
2025-11-10 05:08:55 +0300

    Add CSP/base64 support and favicon 204 route

--- qr-attendance-backend/src/index.js
+++ qr-attendance-backend/src/index.js
@@ -28,7 +28,7 @@ app.use((req, res, next) => {
   res.setHeader(
     "Content-Security-Policy",
     "default-src 'self'; " +
-    "img-src 'self' data:; " +
+    "img-src 'self' data: https://qr-attendance-5crh.onrender.com https://qr-qttendqnce.onrender.com; " +
     "script-src 'self'; " +
     "style-src 'self' 'unsafe-inline'; " +
     "connect-src 'self' " + allowedOrigins.join(' ') + ";"
